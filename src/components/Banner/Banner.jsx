4c58103961c6a9c6f529805bb4382a32c22bc3b9
amirultanim07 <amirultanim@gmail.com>
2025-04-19 08:21:22 -0600

    made website fully responsive

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -10,12 +10,11 @@ const Banner = () => {
                     src={bookimage}
                     className="max-w-sm rounded-lg shadow-2xl" />
                 <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <h1 className="text-5xl font-bold">Books to freshen up your bookshelf</h1>
                     <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                        Provident cupiditate voluptatem et in. In deleniti eaque aut repudiandae et a id nisi.
                     </p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <button className="btn btn-accent">Get Started</button>
                 </div>
             </div>
         </div>
